84d5c5b6d6291ac47ba6cc563c57c0f56ba71a0c
Heisenberg <esfahani.cyrus@gmail.com>
2024-08-11 08:38:32 -0700

    added a License a badge and link to my Markdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,30 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license || license === 'None') {
+    return '';
+  }
+  return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  if (!license || license === 'None') {
+    return '';
+  }
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'GPLv3':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    case 'Apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'BSD 3-Clause':
+      return 'https://opensource.org/licenses/BSD-3-Clause';
+    default:
+      return '';
+  }
 }
 
 // TODO: Create a function that returns the license section of README
